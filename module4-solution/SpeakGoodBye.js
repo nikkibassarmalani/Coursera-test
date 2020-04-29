8c6a83ffe17f6004c3bf70280f4a14a138ad8da0
nikkibassarmalani <63634528+nikkibassarmalani@users.noreply.github.com>
2020-04-29 10:00:59 +0530

    Add files via upload

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function(window){
+
+var speakWord = "Good Bye";
+
+ var byeSpeaker = function speak (names){
+console.log(speakWord+" "+names[i]);
+};
+
+window.byeSpeaker = byeSpeaker;
+
+})(window);
